c5c87b55810033b109bee2f4f9a00e505e1bc2f0
Danut Chereches <load.runner@weednet.ro>
2015-04-13 19:23:36 +0300

    * disabled building unused modules (cocos_studio, cocos_builder, cocos_3d, spime) * added cocosdenshion_statis to android

--- cocos/Android.mk
+++ cocos/Android.mk
@@ -248,12 +248,13 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := cocos2dx_static
 LOCAL_MODULE_FILENAME := libcocos2d
 
-LOCAL_STATIC_LIBRARIES := cocostudio_static
-LOCAL_STATIC_LIBRARIES += cocosbuilder_static
-LOCAL_STATIC_LIBRARIES += cocos3d_static
-LOCAL_STATIC_LIBRARIES += spine_static
+#LOCAL_STATIC_LIBRARIES := cocostudio_static
+#LOCAL_STATIC_LIBRARIES += cocosbuilder_static
+#LOCAL_STATIC_LIBRARIES += cocos3d_static
+#LOCAL_STATIC_LIBRARIES += spine_static
 LOCAL_STATIC_LIBRARIES += cocos_network_static
 LOCAL_STATIC_LIBRARIES += audioengine_static
+LOCAL_STATIC_LIBRARIES += cocosdenshion_static
 
 include $(BUILD_STATIC_LIBRARY)
 #==============================================================
@@ -264,11 +265,11 @@ $(call import-module,jpeg/prebuilt/android)
 $(call import-module,tiff/prebuilt/android)
 $(call import-module,webp/prebuilt/android)
 $(call import-module,chipmunk/prebuilt/android)
-$(call import-module,3d)
+#$(call import-module,3d)
 $(call import-module,audio/android)
-$(call import-module,editor-support/cocosbuilder)
-$(call import-module,editor-support/cocostudio)
-$(call import-module,editor-support/spine)
+#$(call import-module,editor-support/cocosbuilder)
+#$(call import-module,editor-support/cocostudio)
+#$(call import-module,editor-support/spine)
 $(call import-module,network)
 $(call import-module,ui)
 $(call import-module,extensions)
